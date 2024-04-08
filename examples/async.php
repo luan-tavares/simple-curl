87410458812ec1104929f4ecf946c0837889b02d
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2024-04-07 21:58:43 -0300

    v2.1.2

--- examples/async.php
+++ examples/async.php
@@ -7,24 +7,47 @@ use LTL\Curl\Async\CurlMultiple;
 require_once __DIR__ .'/__init.php';
 
 $queries = [
-    'limit' => 1
+    'limit' => 100
 ];
 
 $multiple = new CurlMultiple;
 
 $init = Carbon::now();
 
-for($i = 0; $i < 100;$i++) {
+for($i = 0; $i < 500;$i++) {
     $curl = (new CurlAsync)->addUri('https://api.hubapi.com/crm/v3/objects/contacts')
         ->bearerToken(ENV['TOKEN'])
         ->withHeaders()
         ->addQueries($queries)
         ->get();
-    $multiple->add($curl);
+    $multiple->push($curl);
 }
 
-$multiple->call(function (CurlAsync $curl) {
-    dump($curl->status(), $curl->response());
-});
+$multiple->call();
+
+foreach ($multiple as $key => $value) {
+    if($value->status() === 0) {
+        dump($value->status(), $value->headers());
+    }
+}
+
+while(true) {
+
+    sleep(1);
+
+    if(!$multiple->errors()) {
+        break;
+    }
+
+    $multiple = new CurlMultiple($multiple->errors());
+
+    $multiple->call();
+
+    foreach ($multiple as $key => $value) {
+        dump($value->status());
+    }
+}
+
+
 
 dd(Carbon::now()->diffInMilliseconds($init)/1000);
--- src/Async/CurlMultiple.php
+++ src/Async/CurlMultiple.php
@@ -2,59 +2,125 @@
 
 namespace LTL\Curl\Async;
 
-use Closure;
+use Countable;
 use CurlMultiHandle;
+use Iterator;
 use LTL\Curl\Async\CurlAsync;
 use LTL\Curl\CurlResponse;
 use ReflectionProperty;
 
-class CurlMultiple
+class CurlMultiple implements Iterator, Countable
 {
     /**
      * @var CurlAsync[]
      */
     private array $items;
 
-    private CurlMultiHandle $multipleCurl;
-
     private bool $isConnected;
 
-    public function __construct()
+    private array|null $errors = null;
+
+    private array $data;
+
+    /**
+     * @param CurlAsync[] $listCurlAsync
+     */
+    public function __construct(array|null $listCurlAsync = null)
     {
-        $this->multipleCurl = curl_multi_init();
+        if(is_null($listCurlAsync)) {
+            return;
+        }
+
+        foreach ($listCurlAsync as $curlAsync) {
+            $this->push($curlAsync);
+        }
     }
 
-    public function add(CurlAsync $curlResource): self
+    public function push(CurlAsync $curlResource): self
     {
         $this->items[] = $curlResource;
-        curl_multi_add_handle($this->multipleCurl, $curlResource->getCurl());
+        
         
         return $this;
     }
 
-    /**
-     * @param Closure(CurlAsync $curl): void $fn
-     */
-    public function call(callable $fn): void
+    public function call(): self
     {
+        $multipleCurl = curl_multi_init();
+
+        foreach ($this->items as $curlAsync) {
+            curl_multi_add_handle($multipleCurl, $curlAsync->getCurl());
+        }
+
         do {
-            $status = curl_multi_exec($this->multipleCurl, $active);
+            $status = curl_multi_exec($multipleCurl, $active);
 
             if ($active) {
-                curl_multi_select($this->multipleCurl);
+                curl_multi_select($multipleCurl);
             }
         } while ($active && $status == CURLM_OK);
+
+        $this->isConnected = true;
 
         foreach ($this->items as $curlResource) {
             $response = new CurlResponse($curlResource->getRequest());
 
-            curl_multi_remove_handle($this->multipleCurl, $curlResource->getCurl());
+            curl_multi_remove_handle($multipleCurl, $curlResource->getCurl());
 
             $reflection = new ReflectionProperty($curlResource, 'response');
 
-            $reflection->setValue($curlResource, $response);
+            $object = clone $curlResource;
 
-            $fn($curlResource);
+            $reflection->setValue($object, $response);
+
+            $this->data[] = $object;
+
+            if($object->error()) {
+                $this->errors[] = $object;
+            }
         }
+       
+        return $this;
+    }
+
+    public function errors(): array|false
+    {
+        if(is_null($this->errors)) {
+            return false;
+        }
+      
+        return $this->errors;
+    }
+
+    public function rewind(): void
+    {
+        reset($this->data);
+    }
+    
+    public function current(): CurlAsync
+    {
+        return current($this->data);
+    }
+    
+    public function key(): string|null
+    {
+        return key($this->data);
+    }
+    
+    public function next(): void
+    {
+        next($this->data);
+    }
+    
+    public function valid(): bool
+    {
+        return !is_null($this->key());
+    }
+
+    /** Countable */
+
+    public function count(): int
+    {
+        return count($this->data);
     }
 }
